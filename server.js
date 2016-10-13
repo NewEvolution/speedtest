92834cd7fe8c55fa857dba8b0956264e2b24e0a9
Ryan Tanay <ryantanay@newevolution.org>
2016-10-13 13:50:33 -0500

    Somehow forgot I was using mongodb not PostgreSQL

--- server.js
+++ server.js
@@ -1,30 +1,42 @@
 'use strict';
+/* eslint no-magic-numbers: 0, no-console: 0 */
 
 const express = require('express');
 const app = express();
 
 const path = require('path');
 app.use(require('node-sass-middleware')({
-  src: path.join(__dirname, '/sass'),
-  dest: path.join(__dirname, '/public/styles'),
-  prefix:'/styles',
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public/styles'),
+  prefix: '/styles',
+  outputStye: 'compressed',
   indentedSyntax: true,
-  outputStyle: 'compressed',
   sourceMap: true
 }));
 
+const routes = require('./routes/')
+app.use(routes);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
-  res.send('You found the marble in the oatmeal!');
-});
+const MONGODB_USER = process.env.MONGODB_USER || '';
+const MONGODB_PASS = process.env.MONGODB_PASS || '';
+const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost';
+const MONGODB_PORT = process.env.MONGODB_PORT || 27017;
 
-const db = require('./models/');
-const localPort = 3000;
-const PORT = process.env.PORT || localPort;
-db.sequelize.sync().then(() => {
+const MONGODB_AUTH = MONGODB_USER ? `${MONGODB_USER}:${MONGODB_PASS}@` : '';
+
+const MONGODB_URL = `mongodb://${MONGODB_AUTH}${MONGODB_HOST}:${MONGODB_PORT}/speedtest`;
+
+const mongoose = require('mongoose');
+mongoose.connect(MONGODB_URL);
+
+const PORT = process.env.PORT || 3000;
+mongoose.connection.on('open', (err) => {
+  if (err) throw err;
+
   app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`); // eslint-disable-line no-console
+    console.log(`Node.js server started. listening on port ${PORT}`);
   });
 });
 
