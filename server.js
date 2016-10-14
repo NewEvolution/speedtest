5e49cf956e9871bd9882c81756d701f1b124cc30
Ryan Tanay <ryantanay@newevolution.org>
2016-10-14 13:29:50 -0500

    Added in body-parser to handle JSON posts

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.use(require('node-sass-middleware')({
   sourceMap: true
 }));
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const routes = require('./routes/')
 app.use(routes);
 
