b1a42730d5a4de98faccaf2f2d1737ad528879d7
Ryan Tanay <ryantanay@newevolution.org>
2016-10-17 10:51:58 -0500

    Added controller/route for speedtest to bring in new test information

---
+++ controllers/speedtest.js
@@ -0,0 +1,7 @@
+'use strict'
+
+// const Speedtest = require('../models/speedtest');
+
+module.exports.test = (req, res) => {
+  res.send('I exist');
+};
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ const express = require('express');
 const router = express.Router();
 
 const api = require('../controllers/api');
-
 router.get('/', api.index)
       .post('/', api.new);
+
+const speedtest = require('../controllers/speedtest');
+router.get('/speedtest', speedtest.test);
 
 module.exports = router;
