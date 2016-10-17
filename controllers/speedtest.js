5fb6ba37d2aae373013cc5c420f2cb87c8b029e2
Ryan Tanay <ryantanay@newevolution.org>
2016-10-17 16:37:00 -0500

    Expanded new speedtest adding route

--- controllers/speedtest.js
+++ controllers/speedtest.js
@@ -1,9 +1,24 @@
 'use strict'
 
 // const Speedtest = require('../models/speedtest');
-const execSync = require("child_process").execSync;
+const execSync = require('child_process').execSync;
+const q = require('q')
+
+const speedtest = () => {
+  return execSync('speedtest-cli --simple').toString();
+}
 
 module.exports.test = (req, res) => {
-  const speedy = execSync("speedtest-cli --simple").toString();
-  res.send(speedy);
+  q.fcall(speedtest).then(results => {
+    const resArr = results.split('\n');
+    resArr.pop();
+    // via http://txt2re.com/index-javascript.php3?s=Download:%2082.15%20Mbit/s&3
+    const regex = new RegExp('.*?([+-]?\\d*\\.\\d+)(?![-+0-9\\.])'); // eslint-disable-line no-new
+    const modArr = resArr.map((item) => {
+      return regex.exec(item)[1];
+    });
+    console.log('Value Array:\n', modArr); // eslint-disable-line no-console
+  });
+
+  res.send('Testing...');
 };
