da32663303ee59d8b19014ee0358edaf0b4e5584
Ryan Tanay <ryantanay@newevolution.org>
2016-10-18 10:46:15 -0500

    Completed new speedtest acquisition route

--- controllers/speedtest.js
+++ controllers/speedtest.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// const Speedtest = require('../models/speedtest');
+const Speedtest = require('../models/speedtest');
 const execSync = require('child_process').execSync;
 const q = require('q')
 
@@ -10,14 +10,23 @@ const speedtest = () => {
 
 module.exports.test = (req, res) => {
   q.fcall(speedtest).then(results => {
-    const resArr = results.split('\n');
-    resArr.pop();
     // via http://txt2re.com/index-javascript.php3?s=Download:%2082.15%20Mbit/s&3
-    const regex = new RegExp('.*?([+-]?\\d*\\.\\d+)(?![-+0-9\\.])'); // eslint-disable-line no-new
-    const modArr = resArr.map((item) => {
+    const regex = new RegExp('.*?([+-]?\\d*\\.\\d+)(?![-+0-9\\.])');
+    const rawData = results.split('\n')
+    rawData.pop();
+    const cleanData = rawData.map((item) => {
       return regex.exec(item)[1];
     });
-    console.log('Value Array:\n', modArr); // eslint-disable-line no-console
+
+    const obj = new Speedtest({
+      ping: cleanData[0],
+      download: cleanData[1],
+      upload: cleanData[2]
+    });
+
+    obj.save((err) => {
+      if (err) throw err;
+    });
   });
 
   res.send('Testing...');
