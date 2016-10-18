b8f21157053aace2b71efa1f3c9ac97ef13cbacc
Ryan Tanay <ryantanay@newevolution.org>
2016-10-18 12:10:02 -0500

    Made index route an actual index

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,10 @@
 const express = require('express');
 const router = express.Router();
 
-const api = require('../controllers/api');
-router.get('/', api.index)
-      .post('/', api.new);
+const apiRoutes = require('./api');
+router.use('/api', apiRoutes);
 
-const speedtest = require('../controllers/speedtest');
-router.get('/speedtest', speedtest.test);
+const speedRoutes = require('./speedtest');
+router.use('/speedtest', speedRoutes);
 
 module.exports = router;
