6949daa87cddb8763dae9a473910a3e8af915872
Ryan Tanay <ryantanay@newevolution.org>
2016-10-19 16:11:43 -0500

    Added in week view, working on month view as summary

--- controllers/api.js
+++ controllers/api.js
@@ -3,46 +3,91 @@
 const Speedtest = require('../models/speedtest');
 const moment = require('moment');
 
-module.exports.today = (req, res) => {
-  const now = moment();
-  const today = `${now.year()}-${now.month() + 1}-${now.date()}`;
-  Speedtest
-    .find({ scantime: {$gte: today} })
+const getResults = (startDate, endDate) => {
+  return Speedtest
+    .find({scantime: {$gte: startDate, $lt: endDate}})
     .exec((err, results) => {
       if (err) throw err;
-      res.send(results);
+      return results;
     });
+};
+
+const getAverage = numArray => {
+  let sum;
+  numArray.forEach(value => { sum += value });
+  return sum / numArray.length;
+};
+
+module.exports.today = (req, res) => {
+  const startDate = moment();
+  const endDate = moment(startDate).add(1, 'day');
+  getResults(startDate, endDate)
+    .then(results => res.send(results))
 };
 
 module.exports.year = (req, res) => {
   const startDate = moment(req.params.date, 'YYYY');
-  const endDate = startDate.add(1, 'year');
-  Speedtest
-    .find({ scantime: { $gte: startDate._d, $lt: endDate._d } })
-    .exec((err, results) => {
-      if (err) throw err;
-      res.send(results);
-    });
+  const endDate = moment(startDate).add(1, 'year');
+  getResults(startDate, endDate)
+    .then(results => res.send(results))
 };
 
 module.exports.month = (req, res) => {
   const startDate = moment(req.params.date, 'YYYYMM');
-  const endDate = startDate.add(1, 'month');
-  Speedtest
-    .find({ scantime: { $gte: startDate._d, $lt: endDate._d } })
-    .exec((err, results) => {
-      if (err) throw err;
-      res.send(results);
-    });
+  const endDate = moment(startDate).add(1, 'month');
+  getResults(startDate, endDate)
+    .then(results => {
+      let currentDay;
+      let ping = [];
+      let download = [];
+      let upload = [];
+      const dayTest = {};
+
+      const summarize = () => {
+        dayTest.ping = getAverage(ping);
+        dayTest.download = getAverage(download);
+        dayTest.upload = getAverage(upload);
+        ping = [];
+        download = [];
+        upload = [];
+        dayTest.scantime = moment()
+          .year(currentDay.year())
+          .month(currentDay.month())
+          .date(currentDay.date())
+      };
+
+      const perDayResults = results.map((test, index) => {
+        currentDay = currentDay ? currentDay : moment(test.scantime);
+
+        if (!currentDay.isSame(test.scantime, 'day')) {
+          summarize();
+          currentDay = moment(test.scantime);
+          return dayTest;
+        }
+
+        ping[ping.length] = test.ping;
+        download[download.length] = test.download;
+        upload[upload.length] = test.length;
+
+        if (index === results.length - 1) {
+          summarize();
+          return dayTest;
+        }
+      });
+      res.send(perDayResults);
+    })
+};
+
+module.exports.week = (req, res) => {
+  const startDate = moment(req.params.date, 'YYYYMMDD');
+  const endDate = moment(startDate).add(7, 'days'); // eslint-disable-line no-magic-numbers
+  getResults(startDate, endDate)
+    .then(results => res.send(results))
 };
 
 module.exports.day = (req, res) => {
   const startDate = moment(req.params.date, 'YYYYMMDD');
-  const endDate = startDate.add(1, 'day');
-  Speedtest
-    .find({ scantime: { $gte: startDate._d, $lt: endDate._d } })
-    .exec((err, results) => {
-      if (err) throw err;
-      res.send(results);
-    });
+  const endDate = moment(startDate).add(1, 'day');
+  getResults(startDate, endDate)
+    .then(results => res.send(results))
 };
--- routes/api.js
+++ routes/api.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const api = require('../controllers/api');
 router.get('/day/:date', api.day)
       .get('/month/:date', api.month)
+      .get('/week/:date', api.week)
       .get('/year/:date', api.year)
       .get('/', api.today);
 
