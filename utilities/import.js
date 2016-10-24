02360bb9caf76eda27ce9e08e3edffd85177672c
Ryan Tanay <ryantanay@newevolution.org>
2016-10-24 12:52:20 -0500

    WIP - designing command line app to parse previously acquired speed test log file

--- utilities/import.js
+++ utilities/import.js
@@ -1,6 +1,24 @@
 #!/usr/bin/env node
 'use strict';
 
-const [, , ...input] = process.argv;
+const args = process.argv;
+const requiredArgs = 3;
+const CLIUsageError = 64;
+const fs = require('fs');
 
-console.log(input); // eslint-disable-line no-console
+if (args.length === requiredArgs) {
+  const filepath = args[2];
+  fs.readFile(filepath, 'utf8', (err, data) => {
+    if (err) throw err;
+    const tests = data
+      .split('#')
+      .map(test => test
+        .split('\n')
+        .filter(test => test !== '' && test !== ' '))
+      .filter(test => test.length !== 0)
+    console.log(tests); // eslint-disable-line no-console
+  });
+} else {
+  console.log('usage: import.js </path/to/speedtest.log>') // eslint-disable-line no-console
+  process.exit(CLIUsageError);
+}
