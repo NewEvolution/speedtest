8fa1d388ef477eed682e8130952409efdf534da3
Ryan Tanay <ryantanay@newevolution.org>
2016-10-25 14:56:00 -0500

    Import script brokenly implemented throws error from trying to make a million post calls at once, but the first ~100 or so get in to the database

--- utilities/import.js
+++ utilities/import.js
@@ -2,22 +2,48 @@
 'use strict';
 
 const args = process.argv;
-const requiredArgs = 3;
+const requiredArgs = 4;
 const CLIUsageError = 64;
 const fs = require('fs');
+const request = require('request');
 
 if (args.length === requiredArgs) {
   const filepath = args[2];
   fs.readFile(filepath, 'utf8', (err, data) => {
     if (err) throw err;
-    data.split('#')
-      .map(test => test
+
+    const speedtestObjects = data
+      .split('#')
+      .map(testBlock => testBlock
         .split('\n')
-        .filter(test => test !== '' && test !== ' '))
-      .filter(test => test.length !== 0)
-      .forEach(test => console.log(test)); // eslint-disable-line no-console
+        .filter(testElement => testElement !== '' && testElement !== ' '))
+      .filter(testArr => testArr.length !== 0)
+      .map(testArr => {
+        const testObj = {};
+        testObj.scantime = testArr.shift();
+        const successfulSpeedtestElements = 3;
+        if (testArr.length === successfulSpeedtestElements) {
+          // via http://txt2re.com/index-javascript.php3?s=Download:%2082.15%20Mbit/s&3
+          const regex = new RegExp('.*?([+-]?\\d*\\.\\d+)(?![-+0-9\\.])');
+          const parsedData = testArr.map(testData => regex.exec(testData)[1]);
+          testObj.ping = parsedData[0];
+          testObj.download = parsedData[1];
+          testObj.upload = parsedData[2];
+        } else {
+          testObj.ping = testObj.download = testObj.upload = '0.0';
+        }
+        return testObj
+      });
+
+    const url = args[3];
+    speedtestObjects.forEach((test, index) => {
+      request.post(url, {json: test}, (err, res, body) => {
+        if (err) throw err;
+        console.log('Created:', index, body); // eslint-disable-line no-console
+      });
+    })
   });
 } else {
-  console.log('usage: import.js </path/to/speedtest.log>') // eslint-disable-line no-console
+  console.log('usage: import.js </path/to/testObj.log> <http://url.to.post.to>') // eslint-disable-line no-console
   process.exit(CLIUsageError);
 }
