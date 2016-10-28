0f34a8b1495565c8249ee301e1ff93c6c7ae19c4
Ryan Tanay <ryantanay@newevolution.org>
2016-10-28 15:30:44 -0500

    Set up for React via Webpack front end.

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const HtmlPlugin = require('html-webpack-plugin');
+const HtmlPluginConfig = new HtmlPlugin({
+  template: `${__dirname}/src/index.html`,
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: [
+    './src/app.js'
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, include: `${__dirname}/src`, loader: 'babel-loader'}
+    ]
+  },
+  output: {
+    filename: 'bundled.js',
+    path: `${__dirname}/dist`
+  },
+  plugins: [HtmlPluginConfig]
+};
