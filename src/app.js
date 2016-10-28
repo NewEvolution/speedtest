8ad01016c38e7021a9f34018d93ebb7ba27b8f20
Ryan Tanay <ryantanay@newevolution.org>
2016-10-28 16:12:13 -0500

    Added react file and ignored it

---
+++ src/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+class Test extends React.Component {
+  render() {
+    return(
+      <div>
+        <p>This is insane</p>
+      </div>
+    )
+  };
+};
+
+ReactDOM.render(<Test />, document.getElementById('app'));
