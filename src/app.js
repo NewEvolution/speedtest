41627657b0faab1c865be57422c9f59d06b75f9c
Ryan Tanay <ryantanay@newevolution.org>
2016-10-31 11:47:29 -0500

    Testing out ReactD3 for charting

--- src/app.js
+++ src/app.js
@@ -1,17 +1,32 @@
 'use strict';
 
 const React = require('react');
-
-class Test extends React.Component {
-  render() {
-    return(
-      <div>
-        <p>This is insane</p>
-      </div>
-    )
-  };
-};
-
 const ReactDOM = require('react-dom');
+const LineChart = require('react-d3-basic').LineChart;
 
-ReactDOM.render(<Test />, document.getElementById('app'));
+(() => {
+  const data = [
+          { x: 1, y: 6 },
+          { x: 2, y: 3 },
+          { x: 3, y: 7 },
+          { x: 4, y: 2 },
+          { x: 5, y: 4 },
+        ],
+        chartSeries = [
+          {
+            field: 'y',
+            name: 'Borps',
+            color: '#ffcc00'
+          }
+        ],
+        x = (item) => item.x;
+
+  ReactDOM.render(
+    <LineChart
+      data={data}
+      chartSeries={chartSeries}
+      x={x}
+    />,
+    document.getElementById('app')
+  );
+})()
