8c2a4644c0070c92b6f90066b217278f448e035b
Ryan Tanay <ryantanay@newevolution.org>
2016-10-31 14:07:16 -0500

    Multiple line testing

--- src/app.js
+++ src/app.js
@@ -6,17 +6,22 @@ const LineChart = require('react-d3-basic').LineChart;
 
 (() => {
   const data = [
-          { x: 1, y: 6 },
-          { x: 2, y: 3 },
-          { x: 3, y: 7 },
-          { x: 4, y: 2 },
-          { x: 5, y: 4 },
+          { x: 1, y: 6, z: 5 },
+          { x: 2, y: 3, z: 1 },
+          { x: 3, y: 7, z: 4 },
+          { x: 4, y: 2, z: 2 },
+          { x: 5, y: 4, z: 3 },
         ],
         chartSeries = [
           {
             field: 'y',
             name: 'Borps',
             color: '#ffcc00'
+          },
+          {
+            field: 'z',
+            name: 'Hurbs',
+            color: '#d602ac'
           }
         ],
         x = (item) => item.x;
