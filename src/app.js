70cad9de0335f7303309430f078df245762a8ad1
Ryan Tanay <ryantanay@newevolution.org>
2016-11-18 17:18:35 -0600

    Used array.map to generate all 4 span buttons

--- src/app.js
+++ src/app.js
@@ -188,38 +188,45 @@ const {firstscan, apiurl} = require('./config'),
     onDatesChange,
     isOutsideRange,
     initialVisibleMonth
-  }) =>
-    <div className={'controls'}>
-      <button
-        className={'nav-button'}
-        disabled={startDate.isSameOrBefore(firstScan, 'day')}
-        onClick={() => previous()}
-      >&laquo;</button>
-      <div className={'center-block'}>
-        <div>
-          <SpanButton name={'Day'} range={range} timespan={timespan} />
-          <SpanButton name={'Week'} range={range} timespan={timespan} />
-          <SpanButton name={'Month'} range={range} timespan={timespan} />
-          <SpanButton name={'Year'} range={range} timespan={timespan} />
+  }) => {
+    const spans = ['Day', 'Week', 'Month', 'Year'],
+          spanButtons = spans.map(name =>
+            <SpanButton key={name}
+                        name={name}
+                        range={range}
+                        rimespan={timespan} />
+          );
+    return(
+      <div className={'controls'}>
+        <button
+          className={'nav-button'}
+          disabled={startDate.isSameOrBefore(firstScan, 'day')}
+          onClick={() => previous()}
+        >&laquo;</button>
+        <div className={'center-block'}>
+          <div>
+            {spanButtons}
+          </div>
+          <div>
+            <DateRangePicker
+              startDate={startDate}
+              endDate={endDate}
+              focusedInput={focusedInput}
+              onFocusChange={onFocusChange}
+              onDatesChange={onDatesChange}
+              isOutsideRange={isOutsideRange}
+              initialVisibleMonth={initialVisibleMonth}
+            />
+          </div>
         </div>
-        <div>
-          <DateRangePicker
-            startDate={startDate}
-            endDate={endDate}
-            focusedInput={focusedInput}
-            onFocusChange={onFocusChange}
-            onDatesChange={onDatesChange}
-            isOutsideRange={isOutsideRange}
-            initialVisibleMonth={initialVisibleMonth}
-          />
-        </div>
+        <button
+          className={'nav-button'}
+          disabled={endDate.isSameOrAfter(tomorrow, 'day')}
+          onClick={() => next()}
+        >&raquo;</button>
       </div>
-      <button
-        className={'nav-button'}
-        disabled={endDate.isSameOrAfter(tomorrow, 'day')}
-        onClick={() => next()}
-      >&raquo;</button>
-    </div>;
+    )
+  }
 
   const SpanButton = ({name, range, timespan}) => {
     const active = name.toLowerCase() == range;
