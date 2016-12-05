c43c194d53bade30f9fb2f3ca7550a7fc7157253
Ryan Tanay <ryantanay@newevolution.org>
2016-12-05 10:41:55 -0600

    Reordered routes by time

--- routes/api.js
+++ routes/api.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 const api = require('../controllers/api');
 router.get('/day/:date', api.day)
-      .get('/month/:date', api.month)
       .get('/week/:date', api.week)
+      .get('/month/:date', api.month)
       .get('/year/:date', api.year)
       .get('/range/:sdate/:edate', api.range)
       .get('/', api.today);
