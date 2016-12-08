0dc3bfaabbd9936184d9227f132a71a3ecf3c21e
Ryan Tanay <ryantanay@newevolution.org>
2016-12-08 13:01:28 -0600

    Declaration changing for everybody

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 'use strict';
 
-const express = require('express');
-const app = express();
+const express = require('express'),
+      app = express(),
 
-const bodyParser = require('body-parser');
+      bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const routes = require('./routes/')
+const routes = require('./routes/');
 app.use(routes);
 
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
-const localMongoPort = 27017;
-const MONGODB_USER = process.env.MONGODB_USER || '';
-const MONGODB_PASS = process.env.MONGODB_PASS || '';
-const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost';
-const MONGODB_PORT = process.env.MONGODB_PORT || localMongoPort;
+const localMongoPort = 27017,
+      MONGODB_USER = process.env.MONGODB_USER || '',
+      MONGODB_PASS = process.env.MONGODB_PASS || '',
+      MONGODB_HOST = process.env.MONGODB_HOST || 'localhost',
+      MONGODB_PORT = process.env.MONGODB_PORT || localMongoPort,
 
-const mongodb_auth = MONGODB_USER ? `${MONGODB_USER}:${MONGODB_PASS}@` : '';
+      mongodb_auth = MONGODB_USER ? `${MONGODB_USER}:${MONGODB_PASS}@` : '',
 
-const mongodb_url = `mongodb://${mongodb_auth}${MONGODB_HOST}:${MONGODB_PORT}/speedtest`;
+      mongodb_url = `mongodb://${mongodb_auth}${MONGODB_HOST}:${MONGODB_PORT}/speedtest`,
 
-const mongoose = require('mongoose');
+      mongoose = require('mongoose');
 mongoose.connect(mongodb_url);
 
-const localPort = 3000;
-const PORT = process.env.PORT || localPort;
+const localPort = 3000,
+      PORT = process.env.PORT || localPort;
 mongoose.connection.on('open', (err) => {
   if (err) throw err;
 
